13446f419468629cbbb745210ae8da1d85836f26
Villeman <villeman.arm@gmail.com>
2023-12-13 16:32:53 +0300

    fix: move clear button to to total block

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
-import { useDispatch} from "react-redux"
-import {clear} from "../store/calculatorSlice"
+
 
 
 const Component = () => {
-    const dispatch = useDispatch() 
-    const clearServices = () => dispatch(clear()) 
+    
+    
     
     return (
         <div className="header">
-            <h2>Layout calculator</h2>
-            <button className="header__btn" onClick={clearServices}>Clear</button>
+            <h3>Layout calculator</h3>    
         </div>
     )
 }
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,14 +1,24 @@
-import { useSelector } from "react-redux"
+import {useSelector, useDispatch} from "react-redux"
+import {clear} from "../store/calculatorSlice"
 
 
 const Component = () => {
     const total = useSelector(store => store.calculator.totalCost) 
 
-    
+    const dispatch = useDispatch() 
+    const clearServices = () => dispatch(clear()) 
+
+    const showClearBtn = () => {
+        if (total) {
+            return (<button className="header__btn" onClick={clearServices}>Clear</button>)
+        }
+    }
+
     return (
         <div className="total">
             <span>Total cost:</span>
             <span>{total}</span>
+            {showClearBtn()}  
         </div>
     )
 }
